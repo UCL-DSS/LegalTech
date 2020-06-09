4df2d4086084e30fcaa3725178d33dd4ad28ff02
Bhavya <bhavya0703@gmail.com>
2020-06-09 15:04:55 +0800

    Add Lesson Launchpad

--- Data_Science_For_Lawyers_Course/Lesson_One_-_Getting_Started/lesson_launchpad.R
+++ Data_Science_For_Lawyers_Course/Lesson_One_-_Getting_Started/lesson_launchpad.R
@@ -1,12 +1,19 @@
-
+# Welcome to the Data Science For Lawyers Course!
+# Run these lines of code below to get started. You might need to pause and wait a bit for the packages to install.
+# Hit cmd-Enter (Mac) or ctrl-Enter (Non-Mac) on each line to run the code.
 install.packages("swirlify")
 library(swirlify)
-install.packages(swirl)
+install.packages("swirl")
 library(swirl)
-swirl()
-get_current_lesson()
-swirlify()
-demo_lesson(from = 30)
-lesson_to_html(open_html = TRUE)
-test_lesson()
-warnings()
+install_course()
+
+# To view the answer key to the lesson in your web browser, execute these lines of code:
+# Lesson One answer key
+set_lesson("Lesson One - Getting Started", "Data Science For Lawyers Course")
+lesson_to_html(dest_dir = NULL, open_html = TRUE, keep_rmd = FALSE,
+               quiet = FALSE, install_course = TRUE)
+
+# Most of the lesson will be in the CONSOLE (the window below this one). 
+# However, feel free to save any of your answers in the SCIRPT below here!
+
+# Type code here
