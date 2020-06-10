f0b817d0c7c2ffe09876dbf08e8118534a7ac359
Amanda Lim <64834177+amandashlim@users.noreply.github.com>
2020-06-09 22:00:02 -0700

    Remove lines about opening HTML

--- Data_Science_For_Lawyers_Course/Lesson_One_-_Getting_Started/lesson_launchpad.R
+++ Data_Science_For_Lawyers_Course/Lesson_One_-_Getting_Started/lesson_launchpad.R
@@ -1,19 +1,14 @@
 # Welcome to the Data Science For Lawyers Course!
-# Run these lines of code below to get started. You might need to pause and wait a bit for the packages to install.
+# Run these lines of code below to install some packages to get started. You might need to pause and wait a bit for the packages to install.
 # Hit cmd-Enter (Mac) or ctrl-Enter (Non-Mac) on each line to run the code.
-install.packages("swirlify")
-library(swirlify)
 install.packages("swirl")
 library(swirl)
+
+# To install the Data Science For Lawyers Course, run this line of code and select the desired lesson.swc file when prompted.
 install_course()
 
-# To view the answer key to the lesson in your web browser, execute these lines of code:
-# Lesson One answer key
-set_lesson("Lesson One - Getting Started", "Data Science For Lawyers Course")
-lesson_to_html(dest_dir = NULL, open_html = TRUE, keep_rmd = FALSE,
-               quiet = FALSE, install_course = TRUE)
+# Run this line of code to start swirl and follow the instrutions in the console.
+swirl()
 
 # Most of the lesson will be in the CONSOLE (the window below this one). 
 # However, feel free to save any of your answers in the SCIRPT below here!
-
-# Type code here
