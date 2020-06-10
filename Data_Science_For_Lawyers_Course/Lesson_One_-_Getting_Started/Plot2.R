58c8eac7a31b58ecd28940924c09a16d1df55546
Bhavya <bhavya0703@gmail.com>
2020-06-10 20:02:21 +0800

    Remove files

--- Data_Science_For_Lawyers_Course/Lesson_One_-_Getting_Started/Plot2.R
+++
@@ -1,8 +0,0 @@
-
-year <- c(2008:2017)
-claims <- c(32,45,67,98,112,77,67,98,103,88)
-disputes <- data.frame(year, claims)
-print(disputes)
-plot(disputes)
-#Let's add axis and graph labels
-plot(disputes$year,disputes$claims, type = "p", xlab="Year", ylab="Claims", main = "Claims per Year")
