3e4628e67ae6311d16b4e27aaf5f86714b85669c
Amanda Lim <64834177+amandashlim@users.noreply.github.com>
2020-06-11 01:01:46 -0700

    Delete old version

--- Lesson_1/Plot1.R
+++
@@ -1,7 +0,0 @@
-# Let's create the following dataset of numbers of claims per year:
-year <- c(2008:2017)
-claims <- c(32,45,67,98,112,77,67,98,103,88)
-disputes <- data.frame(year, claims)
-print(disputes)
-# We can visualize that data using the plot() function.
-plot(disputes)
